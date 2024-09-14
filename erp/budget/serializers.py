5bb1438c35b03f2d0c00cc8397d5ae174698c333
wrefinity <wrefordmessi@gmail.com>
2024-09-14 20:42:34 +0100

    budget refinements

--- erp/budget/serializers.py
+++ erp/budget/serializers.py
@@ -2,13 +2,23 @@
 from rest_framework import serializers
 from project.serializers import ProjectSerializer
 from .models import Budget
+from project.models import Project
+
+# class BudgetSerializer(serializers.ModelSerializer):
+#     """budget serializers"""    
+#     project_id = ProjectSerializer()
+    
+#     class Meta:
+#         """budget meta data"""
+#         model = Budget
+#         fields = '__all__'
 
 
 class BudgetSerializer(serializers.ModelSerializer):
-    """budget serializers"""    
-    project_id = ProjectSerializer()
-    
+    # Use PrimaryKeyRelatedField to allow passing just the ID of the project
+    project_id = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all(), source='project')
+
     class Meta:
-        """budget meta data"""
         model = Budget
-        fields = '__all__'
+        fields = ['id', 'project_id', 'budget_position', 'start_date', 'end_date', 'planned_amount', 'practical_amount', 'paid_amount', 'paid_date']
+
